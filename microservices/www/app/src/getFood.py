c0624b7a42a887c57537be70d26678bd1fca0211
Haidar <h.almubarak@ieee.org>
2017-12-02 17:13:09 -0600

    Adding intial getFood.py

--- microservices/www/app/src/getFood.py
+++ microservices/www/app/src/getFood.py
@@ -3,24 +3,30 @@ import json
 import requests
 from .config import dataUrl, dataHeaders
 
+# The package will be accessible by importing clarifai:
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
 get_food = Blueprint('get_food', __name__)
 clarifaiAPIKey = "d2f08a3a17894b3da12cb03f291f9d6c"
+
+app = ClarifaiApp(api_key=clarifaiAPIKey)
+
+# get the general model
+model = app.models.get('food-items-v1.0')
+# predict with the model
+#model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+image = ClImage(url='http://www.bulkingbro.com/wp-content/uploads/2014/11/bulkingbro-pasta.jpg')
+result = model.predict([image])
 
 @get_food.route("/get_food")
 def articles():
 
-    query = {
-        'type': 'select',
-        'args': {
-            'table': 'article',
-            'columns': [
-                '*'
-            ]
-        }
-    }
-    print(json.dumps(query))
+
+    print(json.dumps(result))
     print(dataUrl)
 
-    response = requests.post( dataUrl, data = json.dumps(query), headers = dataHeaders)
+    #response = requests.post( dataUrl, data = json.dumps(query), headers = dataHeaders)
 
-    return response.content
+    return result#response.content
