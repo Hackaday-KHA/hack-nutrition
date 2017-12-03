88a0085a4a1c4b53326d87ba331560f43b55aa00
Haidar <h.almubarak@ieee.org>
2017-12-02 19:07:35 -0600

    select food in url

--- microservices/www/app/src/getNutriton.py
+++ microservices/www/app/src/getNutriton.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 import json
 import requests
+from flask import request
 from .config import dataUrl_nutrition, dataheaders_nutrition
 
 get_nutrition = Blueprint('get_nutrition', __name__)
@@ -8,7 +9,8 @@ get_nutrition = Blueprint('get_nutrition', __name__)
 @get_nutrition.route("/get_nutrition")
 def articles():
 
-    query = { 'query': 'apple' }
+    food = request.args.get('foodType')
+    query = { 'query': food }
 
     #print(json.dumps(query))
     #print(dataUrl_nutrition)
