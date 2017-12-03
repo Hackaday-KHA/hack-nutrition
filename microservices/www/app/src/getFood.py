2cae4458b85c48dec52102e08f5c13f33e351e50
Haidar <h.almubarak@ieee.org>
2017-12-02 20:05:26 -0600

    try new clarifai model

--- microservices/www/app/src/getFood.py
+++ microservices/www/app/src/getFood.py
@@ -3,10 +3,10 @@ import json
 import requests
 from .config import dataUrl, dataHeaders
 
-dataUrl_clarifai = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+dataUrl_clarifai = "https://api.clarifai.com/v2/models/dfebc169854e429086aceb8368662641/outputs"
 dataheaders_clarifai = {
 "Content-Type": "application/json",
-"Authorization": "Key d2f08a3a17894b3da12cb03f291f9d6c"}
+"Authorization": "Key a477319ad56c4fa68a22d52eff5c786e"}
 
 get_food = Blueprint('get_food', __name__)
 clarifaiAPIKey = "d2f08a3a17894b3da12cb03f291f9d6c"
@@ -18,7 +18,7 @@ clarifaiAPIKey = "d2f08a3a17894b3da12cb03f291f9d6c"
 
 @get_food.route("/get_food")
 def articles():
-    query = {"inputs": [{"data": {"image":{"url": "https://samples.clarifai.com/metro-north.jpg"}}}]}
+    query = {"inputs": [{"data": {"image":{"url": "http://www.bulkingbro.com/wp-content/uploads/2014/11/bulkingbro-pasta.jpg"}}}]}
 
     response = requests.post( dataUrl_clarifai, data = json.dumps(query), headers = dataheaders_clarifai)
     result = response.content.decode('latin1')
